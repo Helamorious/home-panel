d28afb0ce9e5e41ff0ac044cb21fd3527647204a
Michael Raison <michael.raison@helamorious.net>
2022-12-29 14:18:14 +1030

    Added MQTT initial config

--- 01-scripts/home-panel.py
+++ 01-scripts/home-panel.py
@@ -3,18 +3,37 @@ from time import sleep
 
 from gpiozero import MotionSensor
 
+import paho.mqtt.client as mqtt
+
 pir = MotionSensor(14)
 
 last_update = time()
 last_reset = last_update
 
-def motion_detected(channel):
+mqtt_server = "mqtt.hilton.local"
+
+def pir_motion_detected(channel):
     print(pir.motion_detected)
     print("Motion detected")
     print(channel)
     global last_reset
 
-pir.when_motion = motion_detected
+def on_mqtt_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("$SYS/#")
+
+def on_mqtt_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_mqtt_connect
+client.on_message = on_mqtt_message
+client.connect(mqtt_server, 1883, 60)
+
+pir.when_motion = pir_motion_detected
 
 while True:
     if time() - last_update >= 10:
