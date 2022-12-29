c63427ebe153590e52e72f95c559169d46cd4d60
Michael Raison <michael.raison@helamorious.net>
2022-12-29 14:11:34 +1030

    Changed main loop

--- 01-scripts/home-panel.py
+++ 01-scripts/home-panel.py
@@ -1,10 +1,23 @@
+from time import time
+from time import sleep
+
 from gpiozero import MotionSensor
 
 pir = MotionSensor(14)
 
-def motion_detected():
+last_update = time()
+last_reset = last_update
+
+def motion_detected(channel):
+    print(pir.motion_detected)
     print("Motion detected")
+    print(channel)
+    global last_reset
 
 pir.when_motion = motion_detected
 
-while(True):
+while True:
+    if time() - last_update >= 10:
+        print("Idle %d secs" % (time() - last_reset))
+        last_update = time()
+    sleep(0.02)
